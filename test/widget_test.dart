15a2708731387292c3b63d67738884f82833ab23
webierta <webierta@gmail.com>
2020-12-07 23:27:50 +0100

    fastlane

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:calling_plan_control/main.dart';
+import 'package:call_counter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
